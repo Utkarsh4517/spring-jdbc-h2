3f037633e07054a486f16385c47703abf3dd3708
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-13 01:01:03 +0530

    project init

---
+++ src/test/java/com/utkarsh/springh2jdbc/SpringH2JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.springh2jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
