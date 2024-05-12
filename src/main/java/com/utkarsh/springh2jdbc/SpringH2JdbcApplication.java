62602085d45297222827756fb6158e8318668d02
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-13 01:21:43 +0530

    created student object using dependency injection

--- src/main/java/com/utkarsh/springh2jdbc/SpringH2JdbcApplication.java
+++ src/main/java/com/utkarsh/springh2jdbc/SpringH2JdbcApplication.java
@@ -1,13 +1,21 @@
 package com.utkarsh.springh2jdbc;
 
+import com.utkarsh.springh2jdbc.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringH2JdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringH2JdbcApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringH2JdbcApplication.class, args);
+        Student s = context.getBean(Student.class);
+        s.setRollNo(49);
+        s.setName("Utkarsh");
+        s.setMarks(89);
+
+
     }
 
 }
--- src/main/java/com/utkarsh/springh2jdbc/model/Student.java
+++ src/main/java/com/utkarsh/springh2jdbc/model/Student.java
@@ -1,5 +1,10 @@
 package com.utkarsh.springh2jdbc.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
 public class Student {
     private int rollNo;
     private String name;
